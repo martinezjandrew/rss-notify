0d04b1441aee0b2c347e3fdb07f5a6313cfb7c37
Andrew Martinez <martinezjandrew@gmail.com>
2025-09-20 00:30:10 -0500

    Added helper function to format body of notification

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use rss::Channel;
 use notify_rust::Notification;
 use chrono::Utc;
+use html2text::from_read;
 
 pub async fn get_feed(link: &str) -> Result<Channel, Box<dyn Error>> {
     let content = reqwest::get(link)
@@ -17,6 +18,7 @@ pub fn send_notify(channel: &Channel) {
     let latest_item = channel.items().get(0);
     let title = latest_item.expect("REASON").title().unwrap();
     let description = latest_item.expect("REASON").description().unwrap();
+    let body = create_body(&feed_title, &description);
     let link = latest_item.expect("REASON").link().unwrap();
 
     println!("Executed notification for \"{feed_title}\" at {time}",
@@ -26,7 +28,7 @@ pub fn send_notify(channel: &Channel) {
 
     Notification::new()
         .summary(title)
-        .body(description)
+        .body(&body)
         .action("default", "default")
         .show()
         .unwrap()
@@ -34,7 +36,34 @@ pub fn send_notify(channel: &Channel) {
             "default" => open::that(link).expect("REASON"),
             "__closed" => println!("the notification was closed"),
             _ => ()
-    });
+        });
 
 
+}
+
+fn create_body(title: &str, description: &str) -> String {
+    let mut plain = from_read(description.as_bytes(), 80).unwrap();
+    if plain.len() > 150 {
+        plain.truncate(150);
+        plain.push_str("...");
+    }
+
+    format!(
+        "~~<i>{}</i>~~\n\n{}\n\nClick to read more 👉",
+        title, plain
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio;
+
+    #[tokio::test]
+    async fn test_send_notify() {
+        let feed_link = "https://archlinux.org/feeds/news/";
+        let feed = get_feed(feed_link).await.unwrap();
+
+        send_notify(&feed);
+    }
 }
